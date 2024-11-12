423783e7c6d033ebb3059647e6d43a10c7f17d31
SupremeAmer <emmyplumbingservice@gmail.com>
2024-11-13 00:22:06 +0100

    Update and rename register.html to script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+{
+  "mode": "live",
+  "tenantId": "6nzz556n",
+  "userId": 25,
+  "userUuid": "cd4b4b1f-a97b-440f-97a2-c5b9b4ba21bc",
+  "sessionId": "9fec94a3-0aa6-4af8-8164-3adfcc018950",
+  "iss": "userfront",
+  "iat": 1731453311469,
+  "exp": 1734045311469,
+  "aliasId": "tenant-alias-id",
+  "authentication": {
+    "firstFactor": {
+      "channel": "email",
+      "strategy": "password"
+    }
+  },
+  "authorization": {
+    "6nzz556n": {
+      "roles": [
+        "admin"
+      ]
+    }
+  },
+  "isConfirmed": true,
+  "isEmailConfirmed": true,
+  "isPhoneNumberConfirmed": true
+}
