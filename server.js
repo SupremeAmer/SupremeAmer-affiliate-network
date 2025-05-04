34440916f545d3b8015ecd889b766d8712704032
SupremeAmer <emmyplumbingservice@gmail.com>
2025-05-04 20:50:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
-dotenv.config();
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+// Example API endpoint
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
-app.get('/', (req, res) => res.send('SupremeAmer Backend is Running!'));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-const authRoutes = require('./routes/auth');
-app.use('/api/auth', authRoutes);
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
